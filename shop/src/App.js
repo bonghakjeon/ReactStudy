f62c9ecf6b3376e3175f6ca4b9f915f876a84e29
DESKTOP-GUHF3TT\bhjeon <bhjeon@imbu.co.kr>
2024-08-27 09:51:54 +0900

    [코딩애플] React 스터디 Part 2 : 쇼핑몰 프로젝트 16강 - 새로운 프로젝트 생성 & Bootstrap 사용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -23,6 +23,7 @@ function App() {
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
+        {/* <Button variant="primary">Primary</Button> */}
         <Container>
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
